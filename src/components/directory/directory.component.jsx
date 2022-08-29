a504a923fb70904decd07fc331fd6012b5018df5
disposable-kiwi <biffboffkev@gmail.com>
2022-08-28 20:10:43 -0400

    built our directory page and refactored scss and component files

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,19 @@
+import CategoryItem from "../category-item/category-item.component";
+import "./directory.styles.scss";
+
+const Directory = (props) => {
+    const {categories} = props;
+    return (
+        <div className="categories-container">
+            {categories.map((category) => {
+                return (<CategoryItem 
+                key={category.id} 
+                title={category.title} 
+                imageUrl={category.imageUrl} />);
+            })}
+        </div>
+    );
+
+}
+
+export default Directory;
