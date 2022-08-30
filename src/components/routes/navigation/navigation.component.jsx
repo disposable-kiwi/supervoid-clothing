0831079ac20850521c20240b68d80bf6d587f44d
disposable-kiwi <biffboffkev@gmail.com>
2022-08-30 00:54:28 -0400

    Added Firebase and Firestore functionality to initialize a database that included a users collection that will be filled with user documents that are populated by anybody signing in through Google. Now we can keep track of who has signed in before, who hasn't, their associated user document and reference attribute, and even create user documents for previously non-existent users

--- src/components/routes/navigation/navigation.component.jsx
+++ src/components/routes/navigation/navigation.component.jsx
@@ -1,7 +1,9 @@
 import { Fragment } from "react";
 import { Outlet, Link } from "react-router-dom";
+import { AppBar } from "@mui/material";
 import {ReactComponent as CrwnLogo} from '../../../assets/crown.svg';
 import './navigation.styles.scss';
+import ResponsiveAppBar from "./appbar.component";
 
 const Navigation = (props) => {
     return (
@@ -10,11 +12,11 @@ const Navigation = (props) => {
                 <Link className="logo-container" to="/">
                     <CrwnLogo className="logo"/>
                 </Link>
-                <div className='links-container'>
-                    <Link className="nav-links-container" to='/shop'>
+                <div className='nav-links-container'>
+                    <Link className="nav-links" to='/shop'>
                         Shop
                     </Link>
-                    <Link className="nav-links-container" to="sign-in">
+                    <Link className="nav-links" to="sign-in">
                         Sign In
                     </Link>
                 </div>
