0f726ac73778eec7d4cf0369741bc7493012e79d
disposable-kiwi <biffboffkev@gmail.com>
2022-09-16 00:39:07 -0400

    populating correct product categories for astronauts

--- src/components/routes/home/home.component.jsx
+++ src/components/routes/home/home.component.jsx
@@ -2,33 +2,34 @@ import { Outlet } from "react-router-dom";
 import Directory from "../../directory/directory.component";
 import '../../../categories.styles.scss';
 
+
 const Home = (props) => {
 
   const categories = [
     {
       "id": 1,
-      "title": "hats",
-      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
+      "title": "Helmets",
+      "imageUrl": "https://img1.cgtrader.com/items/3100866/e71f31c890/large/astronaut-helmet-3d-model-low-poly-obj-fbx-stl-blend.jpg"
     },
     {
       "id": 2,
-      "title": "jackets",
-      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
+      "title": "Gloves",
+      "imageUrl": "https://images.halloweencostumes.com/products/5747/1-1/kids-astronaut-gloves.jpg"
     },
     {
       "id": 3,
-      "title": "sneakers",
-      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
+      "title": "Upper Body",
+      "imageUrl": "https://i.ytimg.com/vi/VsdoJy8rzZg/mqdefault.jpg"
     },
     {
       "id": 4,
-      "title": "womens",
-      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
+      "title": "Pants",
+      "imageUrl": "https://www.renderhub.com/albin/emu-nasa-space-suit/emu-nasa-space-suit-07.jpg"
     },
     {
       "id": 5,
-      "title": "mens",
-      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
+      "title": "Boots",
+      "imageUrl": "http://cdn.shopify.com/s/files/1/0251/7054/2669/products/Astronaut-boot-plush-slippers-nasa-space-boots-ground-up-international-adult-mens-womens-bedroom-01_1024x1024.jpg?v=1612818582"
     }
   ];
 
